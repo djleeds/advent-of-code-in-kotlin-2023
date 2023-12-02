3f0da2f8e70da55963969dcd399303b51da3f4de
Dave Leeds <dave@typealias.com>
2023-12-02 13:57:48 -0600

    Solve Day 2 Part 2

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -8,6 +8,8 @@ data class Draw(val red: Int, val green: Int, val blue: Int) {
     fun isPossible(fullDraw: Draw) =
         red <= fullDraw.red && green <= fullDraw.green && blue <= fullDraw.blue
 
+    val power = red * green * blue
+
     companion object {
         fun parse(input: String): Draw =
             input.split(",").map { it.trim().split(" ") }
@@ -23,6 +25,9 @@ data class Draw(val red: Int, val green: Int, val blue: Int) {
 
 data class Game(val number: Int, val draws: List<Draw>) {
     fun isPossible(fullDraw: Draw) = draws.all { it.isPossible(fullDraw) }
+
+    fun smallestFullDraw() =
+        Draw(draws.maxOf { it.red }, draws.maxOf { it.green }, draws.maxOf { it.blue })
 
     companion object {
         fun parse(input: String): Game {
@@ -40,18 +45,22 @@ fun main() {
     fun part1(input: List<String>) =
         Game.parseMany(input).filter { it.isPossible(Draw(12, 13, 14)) }.sumOf { it.number }
 
-    fun part2(input: List<String>): Int {
-        return 1
-    }
+    fun part2(input: List<String>) =
+        Game.parseMany(input).map { it.smallestFullDraw() }.sumOf { it.power }
 
-    val part1Test = part1(readInput("day02/test-input-1"))
-    println(part1Test)
-    assertThat(part1Test).isEqualTo(8)
+    val part1TestResult = part1(readInput("day02/test-input-1"))
+    println(part1TestResult)
+    assertThat(part1TestResult).isEqualTo(8)
 
-    val input = readInput("day02/input")
-    println(part1(input))
-    assertThat(part1(input)).isEqualTo(1867)
-//
-//    part2(input).println()
-//    assertThat(part2(input)).isEqualTo(53221)
+    val part1Result = part1(readInput("day02/input"))
+    println(part1Result)
+    assertThat(part1Result).isEqualTo(1867)
+
+    val part2TestResult = part2(readInput("day02/test-input-1"))
+    println(part2TestResult)
+    assertThat(part2TestResult).isEqualTo(2286)
+
+    val part2Result = part2(readInput("day02/input"))
+    println(part2Result)
+    assertThat(part2Result).isEqualTo(84538)
 }
