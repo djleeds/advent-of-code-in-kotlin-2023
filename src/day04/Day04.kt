fb11b7aae52981addae76dde43bea82df4b87e52
Dave Leeds <dave@typealias.com>
2023-12-04 14:53:48 -0600

    Solve Day 4 Part 2

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -3,32 +3,46 @@ package day04
 import solve
 import kotlin.math.pow
 
-data class Card(val id: Int, val winners: List<Int>, val numbers: List<Int>) {
-    private val winningCount: Int = numbers.count { it in winners }
-    val points get() = 2.toDouble().pow(winningCount - 1).toInt()
+data class Card(
+    val id: Int,
+    val winners: List<Int>,
+    val numbers: List<Int>,
+    val copiesWon: List<Card> = emptyList()
+) {
+    val winningCount: Int = numbers.count { it in winners }
+    val points = 2.toDouble().pow(winningCount - 1).toInt()
+    val total: Int = 1 + copiesWon.sumOf { it.total }
 }
-
 
 fun main() {
     fun part1(input: List<String>): Int = parse(input).sumOf { it.points }
-
-    fun part2(input: List<String>): Int = -1
+    fun part2(input: List<String>): Int = parse(input).sumOf { it.total }
 
     solve(::part1, withInput = "day04/test", andAssert = 13)
     solve(::part1, withInput = "day04/input", andAssert = 23673)
 
-    //solve(::part2, withInput = "day04/test", andAssert = null)
-    //solve(::part2, withInput = "day04/input", andAssert = null)
+    solve(::part2, withInput = "day04/test", andAssert = 30)
+    solve(::part2, withInput = "day04/input", andAssert = 12263631)
 }
 
-fun parse(input: List<String>): List<Card> =
-    input.map { line ->
+fun <T> List<T>.duplicate() = ArrayList(this)
+
+fun parse(input: List<String>): List<Card> {
+    val cards = mutableListOf<Card>()
+    for (i in input.indices.reversed()) {
+        val line = input[i]
         val (label, values) = line.split(":")
         val (_, id) = label.split(" ").filter { it.isNotEmpty() }
         val (winners, numbers) = values.split("|")
-        Card(
+        val card = Card(
             id.toInt(),
             winners.split(" ").filter { it.isNotEmpty() }.map { it.toInt() },
             numbers.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
-        )
+        ).let {
+            if (it.winningCount == 0) it else
+                it.copy(copiesWon = cards.subList(cards.size - it.winningCount, cards.size).duplicate())
+        }
+        cards.add(card)
     }
+    return cards.reversed()
+}
